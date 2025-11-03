6718bdcb1fc166fd4c813bc70dd33d2870461306
amirjavadnia <ajavadnia4443@sdsu.edu>
2025-11-03 10:43:36 -0800

    Update heap.h

--- heap.h
+++ heap.h
@@ -15,17 +15,23 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+   data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
+      if (size == 0) return -1;
+        int smallest = data[0];
+        
         return -1; // placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+             
     }
 
     void downheap(int pos, int weightArr[]) {
